5e2808f81dabc6d19cdf01af3c6520dced004c20
Sudip-Magar <sudeepmagar730@gmail.com>
2025-10-31 15:51:57 +0545

    header part completed

--- app/Livewire/Admin/Dashboard.php
+++ app/Livewire/Admin/Dashboard.php
@@ -2,8 +2,10 @@
 
 namespace App\Livewire\Admin;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
+#[Layout('components.layouts.admin')]
 class Dashboard extends Component
 {
     public function render()
---
+++ app/Livewire/Admin/Vendor.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use Livewire\Component;
+
+class Vendor extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.vendor');
+    }
+}
