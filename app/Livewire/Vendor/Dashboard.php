42b443733832def563e3552429d11700fe1813d3
Sudip-Magar <sudeepmagar730@gmail.com>
2025-11-05 14:53:11 +0545

    dashboard page ui redesigned and chartjs bug solved

--- app/Livewire/Vendor/Dashboard.php
+++ app/Livewire/Vendor/Dashboard.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Vendor;
 
+use App\Models\Order_item;
 use App\Models\Product;
 use App\Models\VendorOrder;
 use Illuminate\Support\Facades\Auth;
@@ -24,10 +25,16 @@ class Dashboard extends Component
         $products = Product::where('vendor_id', $vendorId)->get();
         $orders = VendorOrder::where('vendor_id',$vendorId)->get();
         $recentOrders = VendorOrder::where('vendor_id',$vendorId)->with('order')->latest()->take(5)->get();
+        $report = Order_item::whereIn('vendor_order_id',$orders->pluck('id'))->selectRaw('product_id, SUM(quantity) as total_sold, SUM(total) as total_price')
+        ->groupBy('product_id')
+        ->orderByDesc('total_sold')
+        ->orderByDesc('total_price')
+        ->get();
         return view('livewire.vendor.dashboard', [
             'products' => $products,
             'orders' => $orders,
             'recentOrders' =>$recentOrders,
+            'report' => $report,
         ]);
     }
 }
